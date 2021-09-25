d9f78af8227342f1955111af3ee57ac96f2ee7f1
Richard Robbins <RERobbins@iTinker.net>
2021-09-25 17:03:47 -0500

    Add some rudimentary exception handling.

--- main.py
+++ main.py
@@ -2,18 +2,25 @@
 import subprocess
 import json
 
-response = subprocess.Popen('speedtest -f json', shell=True, stdout=subprocess.PIPE).stdout.read()
+while True:
+    try:
+        response = subprocess.Popen(
+            "speedtest -f json", shell=True, stdout=subprocess.PIPE
+        ).stdout.read()
+        break
+    except Exception as exception:
+        print(exception)
+        continue
 
 response_dict = json.loads(response)
 
-print (f"Timestamp: {response_dict['timestamp']}")
-print (f"Ping: jitter: {response_dict['ping']['jitter']} latency: {response_dict['ping']['latency']}")
-print (f"Download: bandwidth: {response_dict['download']['bandwidth']}")
-print (f"Download: bytes: {response_dict['download']['bytes']}")
-print (f"Download: elapsed: {response_dict['download']['elapsed']}")
-print (f"Upload: bandwidth: {response_dict['upload']['bandwidth']}")
-print (f"Upload: bytes: {response_dict['upload']['bytes']}")
-print (f"Upload: elapsed: {response_dict['upload']['elapsed']}")
-
-
-
+print(f"Timestamp: {response_dict['timestamp']}")
+print(
+    f"Ping: jitter: {response_dict['ping']['jitter']} latency: {response_dict['ping']['latency']}"
+)
+print(f"Download: bandwidth: {response_dict['download']['bandwidth']}")
+print(f"Download: bytes: {response_dict['download']['bytes']}")
+print(f"Download: elapsed: {response_dict['download']['elapsed']}")
+print(f"Upload: bandwidth: {response_dict['upload']['bandwidth']}")
+print(f"Upload: bytes: {response_dict['upload']['bytes']}")
+print(f"Upload: elapsed: {response_dict['upload']['elapsed']}")
